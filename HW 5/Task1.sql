a11c5d181dafca7545b8f1e30331c88903e92fbf
komal-shahid <komal.shahid@city.ac.uk>
2024-11-17 17:06:44 +0000

    Update Task1.sql

--- HW 5/Task1.sql
+++ HW 5/Task1.sql
@@ -1,12 +1,12 @@
---1
+-- 1
 SELECT bill_date, bill_total FROM restbill WHERE cust_name = "Bob Crow";
---2 
+-- 2 
 SELECT DISTINCT cust_name FROM restbill WHERE cust_name LIKE "%Smith" ORDER BY cust_name DESC;
---3
-SELECT DISTINCT cust_name FROM restbill WHERE cust_name LIKE "%C%";
---4
-SELECT DISTINCT first_name FROM reststaff WHERE headwaiter IS NULL;
---5 
+-- 3
+SELECT DISTINCT cust_name FROM restbill WHERE cust_name LIKE "% C%";
+-- 4
+SELECT DISTINCT first_name as "Headwaiter name" FROM reststaff WHERE headwaiter IS NULL;
+-- 5 
 SELECT * FROM restbill WHERE bill_date LIKE "__0216";
---6
-SELECT bill_date FROM restbill WHERE bill_date LIKE "%15" ORDER BY DESC;
+-- 6
+SELECT DISTINCT bill_date FROM restbill WHERE bill_date LIKE "____15" ORDER BY bill_date DESC;
