dadf6e9dd24687c1cc9428e5317ab22ed8d397b4
komal-shahid <komal.shahid@city.ac.uk>
2024-11-17 17:30:31 +0000

    Create task2.sql

---
+++ HW 5/task2.sql
@@ -0,0 +1,6 @@
+INSERT INTO petevent VALUES ('Fluffy','2020-10-15','vet','antibiotics')
+INSERT INTO petevent VALUES ('Hammy','2020-10-15','vet','antibiotics')
+INSERT INTO petPet VALUES ('Hammy','hamster','M','2020-10-30')
+INSERT INTO petevent VALUES ('Claws','1997-08-03','vet','broke rib')
+INSERT INTO petevent VALUES ('Puffball','2020-09-02','vet','death')
+DELETE FROM petPet WHERE owner = 'Harold' AND species = 'dog'
